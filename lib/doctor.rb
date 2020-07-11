b5faf18ca2377099c1179f2db063793bddbe4182
Cameron Hudson <camnn3@yahoo.com>
2020-07-11 18:59:49 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,4 +11,16 @@ class Doctor
     @name = name
     @@all << self
   end  
+  
+  def appointments
+    Appointments.all.select {|appt| appt.docter == self}
+  end
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+  
+  def patients
+    appointments.map {|appt| appt.patient}
+  end
 end
