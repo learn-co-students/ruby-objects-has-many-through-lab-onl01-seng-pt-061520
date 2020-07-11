8dacfced2c09835eee4605098e676830acbddc75
Cameron Hudson <camnn3@yahoo.com>
2020-07-11 19:06:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,14 +3,14 @@ class Artist
   
   @@all = []
   
+  def self.all
+    @@all
+  end
+  
   def initialize(name)
     @name = name
     @@all << self
   end  
-  
-  def self.all
-    @@all
-  end
   
   def songs
     Song.all.select {|song| song.artist == self}
