4de0c64d44ca47081147eb8cd8711156f14b34d0
Cameron Hudson <camnn3@yahoo.com>
2020-07-11 19:00:41 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -11,4 +11,12 @@ class Genre
     @name = name
     @@all << self
   end  
+  
+  def songs
+    Song.all.select {|song| song.genre == self}
+  end
+  
+  def artists
+    songs.map {|song| song.artist}
+  end
 end
